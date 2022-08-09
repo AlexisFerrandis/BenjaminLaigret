421483839d9e828c0781a7fcc94de6b34a5c3a6a
Alexis Ferrandis <alexis.1.ferrandis@gmail.com>
2022-08-09 13:47:04 +0200

    Portfolio v1

--- client/src/components/home/Portfolio/Grid.js
+++ client/src/components/home/Portfolio/Grid.js
@@ -1,28 +1,28 @@
 import React from "react";
 
 // img
-import grid0 from "../../../assets/portfolio/grid0.jpg";
-import grid1 from "../../../assets/portfolio/grid1.jpg";
-import grid2 from "../../../assets/portfolio/grid2.jpg";
-import grid3 from "../../../assets/portfolio/grid3.jpg";
-import grid4 from "../../../assets/portfolio/grid4.jpg";
-import grid5 from "../../../assets/portfolio/grid5.jpg";
-import grid6 from "../../../assets/portfolio/grid6.jpg";
-import grid7 from "../../../assets/portfolio/grid7.jpg";
-import grid8 from "../../../assets/portfolio/grid8.jpg";
-import grid9 from "../../../assets/portfolio/grid9.jpg";
-import grid10 from "../../../assets/portfolio/grid10.jpg";
-import grid11 from "../../../assets/portfolio/grid11.jpg";
-import grid12 from "../../../assets/portfolio/grid12.jpg";
-import grid13 from "../../../assets/portfolio/grid13.jpg";
-import grid14 from "../../../assets/portfolio/grid14.jpg";
-import grid15 from "../../../assets/portfolio/grid15.jpg";
-import grid16 from "../../../assets/portfolio/grid16.jpg";
-import grid17 from "../../../assets/portfolio/grid17.jpg";
-import grid18 from "../../../assets/portfolio/grid18.jpg";
-import grid19 from "../../../assets/portfolio/grid19.jpg";
-import grid20 from "../../../assets/portfolio/grid20.jpg";
-import grid21 from "../../../assets/portfolio/grid21.jpg";
+import grid0 from "../../../assets/portfolio/grid0-min.jpg";
+import grid1 from "../../../assets/portfolio/grid1-min.jpg";
+import grid2 from "../../../assets/portfolio/grid2-min.jpg";
+import grid3 from "../../../assets/portfolio/grid3-min.jpg";
+import grid4 from "../../../assets/portfolio/grid4-min.jpg";
+import grid5 from "../../../assets/portfolio/grid5-min.jpg";
+import grid6 from "../../../assets/portfolio/grid6-min.jpg";
+import grid7 from "../../../assets/portfolio/grid7-min.jpg";
+import grid8 from "../../../assets/portfolio/grid8-min.jpg";
+import grid9 from "../../../assets/portfolio/grid9-min.jpg";
+import grid10 from "../../../assets/portfolio/grid10-min.jpg";
+import grid11 from "../../../assets/portfolio/grid11-min.jpg";
+import grid12 from "../../../assets/portfolio/grid12-min.jpg";
+import grid13 from "../../../assets/portfolio/grid13-min.jpg";
+import grid14 from "../../../assets/portfolio/grid14-min.jpg";
+import grid15 from "../../../assets/portfolio/grid15-min.jpg";
+import grid16 from "../../../assets/portfolio/grid16-min.jpg";
+import grid17 from "../../../assets/portfolio/grid17-min.jpg";
+import grid18 from "../../../assets/portfolio/grid18-min.jpg";
+import grid19 from "../../../assets/portfolio/grid19-min.jpg";
+import grid20 from "../../../assets/portfolio/grid20-min.jpg";
+import grid21 from "../../../assets/portfolio/grid21-min.jpg";
 
 const Grid = ({ setFullScreen, setPicSrc }) => {
 	const handleFullScreenPic = (e) => {
@@ -38,66 +38,66 @@ const Grid = ({ setFullScreen, setPicSrc }) => {
 			<div id="item-1" className="grid-item" onClick={(e) => handleFullScreenPic(e)}>
 				<img src={grid1} alt="illustration" className="portoflio-illustration" />
 			</div>
-			<div id="item-2" className="grid-item">
+			<div id="item-2" className="grid-item" onClick={(e) => handleFullScreenPic(e)}>
 				<img src={grid2} alt="illustration" className="portoflio-illustration" />
 			</div>
-			<div id="item-3" className="grid-item">
+			<div id="item-3" className="grid-item" onClick={(e) => handleFullScreenPic(e)}>
 				<img src={grid3} alt="illustration" className="portoflio-illustration" />
 			</div>
-			<div id="item-4" className="grid-item">
+			<div id="item-4" className="grid-item" onClick={(e) => handleFullScreenPic(e)}>
 				<img src={grid4} alt="illustration" className="portoflio-illustration" />
 			</div>
-			<div id="item-5" className="grid-item">
+			<div id="item-5" className="grid-item" onClick={(e) => handleFullScreenPic(e)}>
 				<img src={grid5} alt="illustration" className="portoflio-illustration" />
 			</div>
-			<div id="item-5bis" className="grid-item">
+			<div id="item-5bis" className="grid-item" onClick={(e) => handleFullScreenPic(e)}>
 				<img src={grid6} alt="illustration" className="portoflio-illustration" />
 			</div>
-			<div id="item-6" className="grid-item">
+			<div id="item-6" className="grid-item" onClick={(e) => handleFullScreenPic(e)}>
 				<img src={grid7} alt="illustration" className="portoflio-illustration" />
 			</div>
 
-			<div id="item-7" className="grid-item">
+			<div id="item-7" className="grid-item" onClick={(e) => handleFullScreenPic(e)}>
 				<img src={grid8} alt="illustration" className="portoflio-illustration" />
 			</div>
-			<div id="item-8" className="grid-item">
+			<div id="item-8" className="grid-item" onClick={(e) => handleFullScreenPic(e)}>
 				<img src={grid9} alt="illustration" className="portoflio-illustration" />
 			</div>
-			<div id="item-9" className="grid-item">
+			<div id="item-9" className="grid-item" onClick={(e) => handleFullScreenPic(e)}>
 				<img src={grid10} alt="illustration" className="portoflio-illustration" />
 			</div>
-			<div id="item-10" className="grid-item">
+			<div id="item-10" className="grid-item" onClick={(e) => handleFullScreenPic(e)}>
 				<img src={grid11} alt="illustration" className="portoflio-illustration" />
 			</div>
-			<div id="item-11" className="grid-item">
+			<div id="item-11" className="grid-item" onClick={(e) => handleFullScreenPic(e)}>
 				<img src={grid12} alt="illustration" className="portoflio-illustration" />
 			</div>
-			<div id="item-12" className="grid-item">
+			<div id="item-12" className="grid-item" onClick={(e) => handleFullScreenPic(e)}>
 				<img src={grid13} alt="illustration" className="portoflio-illustration" />
 			</div>
-			<div id="item-13" className="grid-item">
+			<div id="item-13" className="grid-item" onClick={(e) => handleFullScreenPic(e)}>
 				<img src={grid14} alt="illustration" className="portoflio-illustration" />
 			</div>
 
-			<div id="item-14" className="grid-item">
+			<div id="item-14" className="grid-item" onClick={(e) => handleFullScreenPic(e)}>
 				<img src={grid15} alt="illustration" className="portoflio-illustration" />
 			</div>
-			<div id="item-15" className="grid-item">
+			<div id="item-15" className="grid-item" onClick={(e) => handleFullScreenPic(e)}>
 				<img src={grid16} alt="illustration" className="portoflio-illustration" />
 			</div>
-			<div id="item-16" className="grid-item">
+			<div id="item-16" className="grid-item" onClick={(e) => handleFullScreenPic(e)}>
 				<img src={grid17} alt="illustration" className="portoflio-illustration" />
 			</div>
-			<div id="item-17" className="grid-item">
+			<div id="item-17" className="grid-item" onClick={(e) => handleFullScreenPic(e)}>
 				<img src={grid18} alt="illustration" className="portoflio-illustration" />
 			</div>
-			<div id="item-18" className="grid-item">
+			<div id="item-18" className="grid-item" onClick={(e) => handleFullScreenPic(e)}>
 				<img src={grid19} alt="illustration" className="portoflio-illustration" />
 			</div>
-			<div id="item-19" className="grid-item">
+			<div id="item-19" className="grid-item" onClick={(e) => handleFullScreenPic(e)}>
 				<img src={grid20} alt="illustration" className="portoflio-illustration" />
 			</div>
-			<div id="item-20" className="grid-item">
+			<div id="item-20" className="grid-item" onClick={(e) => handleFullScreenPic(e)}>
 				<img src={grid21} alt="illustration" className="portoflio-illustration" />
 			</div>
 		</>
